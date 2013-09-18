9af417b96b202328202f887a88ac98d8af034625
OKso <okso.me>
2013-09-18 19:58:11 +0200

    Added line drawing and offset support.

--- words.py
+++ words.py
@@ -3,7 +3,18 @@
     alphabet grids.
 '''
 
+import random
+
 import string
 alphabet = string.lowercase
 
 words = [word.strip() for word in open('/usr/share/dict/words').readlines()]
+
+def new_line(offset=0):
+    line = alphabet
+    line = line[offset:] + line[:offset]
+    return line
+
+for i in xrange(10):
+    offset = random.randrange(len(alphabet))
+    print new_line(offset=offset), 'o%02i' % offset
